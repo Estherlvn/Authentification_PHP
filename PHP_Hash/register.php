ac0fd4adf09feb334f54f130a6be100cf8d419a8
Estherlvn <estherlvn@gmail.com>
2025-01-07 12:02:52 +0100

    update login et register

--- PHP_Hash/register.php
+++ PHP_Hash/register.php
@@ -21,8 +21,8 @@
 
         <label for="pass2">Confirmation du mot de passe</label>
         <input type="password" name="pass2" id="pass2"><br>
-        
-        <input type="submit" value="S'enregistrer">
+
+        <input type="submit" name="submit" value="S'enregistrer">
 
     </form>
     
--- PHP_Hash/traitement.php
+++ PHP_Hash/traitement.php
@@ -1,50 +1,72 @@
 <?php
 
+session_start();
+
 if (isset($_GET["action"])) {
+
     switch ($_GET["action"]) {
         case "register":
-            // Connexion BDD
-            $pdo = new PDO("mysql:host=localhost;dbname=forum_ledlev;charset=utf8", "root", "");
 
-            // Filtrer la saisie des champs du formulaire d'inscription
-            $pseudo = filter_input(INPUT_POST, "pseudo", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL);
-            $pass1 = filter_input(INPUT_POST, "pass1", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $pass2 = filter_input(INPUT_POST, "pass2", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            if (isset($_POST["submit"])) {  // SI le formulaire est soumis ...alors
+                // Connexion à la BDD
+                $pdo = new PDO("mysql:host=localhost;dbname=forum_ledlev;charset=utf8", "root", "");
 
-            // Vérifier la validité des champs
-            if ($pseudo && $email && $pass1 && $pass2) {
-                    // var_dump("ok"); die;
+                // Filtrer la saisie des champs du formulaire d'inscription
+                $pseudo = filter_input(INPUT_POST, "pseudo", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+                $pass1 = filter_input(INPUT_POST, "pass1", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $pass2 = filter_input(INPUT_POST, "pass2", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+                // Vérifier la validité des champs
+                if ($pseudo && $email && $pass1 && $pass2) {
                     $requete = $pdo->prepare("SELECT * FROM membre WHERE email = :email");
                     $requete->execute(["email" => $email]);
-                    $user = $requete->fetch(); // résultat de la requete (si le user existe)
-                    // si l'utilisateur (user) existe : redirection vers la page register.php
-                    if($user) {
-                        header("location: register.php"); exit;
-                    // si l'utilisateur n'existe pas
+                    $user = $requete->fetch(); // résultat de la requête (si le membre existe)
+
+                    // Si l'utilisateur (membre) existe : redirection vers la page register.php
+                    if ($user) {
+                        header("Location: register.php");
+                        exit;
                     } else {
-                        // var_dump("Utilisateur inexistant"); die;
-                    // insertion de l'utilisateur dans la BDD
-                        if($pass1 == $pass2 && strlen($pass1) >= 5) {    // vérifier que les mdp sont identiques + dépasse 5 char.
-                        $insertUser = $pdo->prepare("INSERT INTO user (pseudo, email, password) VALUES (:pseudo, :email, :password)");
-                        $insertUser->execute([
-                            "pseudo" => $pseudo,
-                            "email" => $email,
-                            "password" => $password_hash($pass1, PASSWORD_DEFAULT)
-                        ]);
-                        header("Location: login.php"); exit;
-                    } else {
-                        // message d'erreur "les mdp ne sont pas identiques"
+                        // Insertion de l'utilisateur dans la BDD
+                        if ($pass1 === $pass2 && strlen($pass1) >= 5) {
+                            $insertUser = $pdo->prepare("INSERT INTO membre (pseudo, email, password) VALUES (:pseudo, :email, :password)");
+                            $insertUser->execute([
+                                "pseudo" => $pseudo,
+                                "email" => $email,
+                                "password" => password_hash($pass1, PASSWORD_DEFAULT)
+                            ]);
+                            header("Location: login.php");
+                            exit;
+                        } else {
+                            // Les MDP ne match pas
+                        }
                     }
-            } 
-        } else {
-            // problème de saisie dans les champs de formulaire
-        }
-
+                } else {
+                   // Problème de saisie dans les champs du formulaire lors de la soumission
+                }
+            } else {
+                
+                // Par défaut, j'affiche le formulaire d'inscription
+                header("Location: register.php");
+                exit;
+            }
             break;
 
-            case "login":
-                // connexion à l'application
+            case "login": // Connexion à l'application
+
+                if($_POST["submit"]) {
+                    // Connexion à la BDD
+                    $pdo = new PDO("mysql:host=localhost;dbname=forum_ledlev;charset=utf8", "root", "");
+                    // on part du principe que la page login ne demande qu'un mail et un mdp
+                    $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+                    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                }
+
+                header("Location: login.php"); exit;
             break;
+
+            case "logout":
+            exit;
     }
 }
